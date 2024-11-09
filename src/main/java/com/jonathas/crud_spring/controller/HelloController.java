93e2e6186578d5efcfbbac7f964a4dc00a4124a7
jonathas <jonathaspereira139@gmail.com>
2024-11-08 21:29:12 -0300

    aula14final

---
+++ src/main/java/com/jonathas/crud_spring/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.jonathas.crud_spring.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/api/hello")
+
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello!World1";
+    }
+
+}
