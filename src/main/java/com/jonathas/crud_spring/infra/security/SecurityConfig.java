333a9b05bc6271c7972c0cbea28896a2642912af
jonathas <jonathaspereira139@gmail.com>
2024-12-03 10:56:20 -0300

    atualização 3-12

--- src/main/java/com/jonathas/crud_spring/infra/security/SecurityConfig.java
+++ src/main/java/com/jonathas/crud_spring/infra/security/SecurityConfig.java
@@ -34,8 +34,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/courses").permitAll()
-                .anyRequest().permitAll()
+                .anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
